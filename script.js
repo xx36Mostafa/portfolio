fdb9b9be19edcb29c9546b05a753a2b2767ddb0f
xx36Mostafa <114435075+xx36Mostafa@users.noreply.github.com>
2024-09-13 04:51:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function shuffle() {
+    const textarea = document.getElementById("shuffledata");
+    const lines = textarea.value.trim().split('\n');
+    for (let i = lines.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [lines[i], lines[j]] = [lines[j], lines[i]];
+    }
+    textarea.value = lines.join('\n');
+}
+function CopyToClipboard() {
+    const textarea = document.getElementById("shuffledata");
+    navigator.clipboard.writeText(textarea.value);
+}
+function RemoveDuplicated() {
+    const textarea = document.getElementById("shuffledata");
+    const duplicated = [...new Set(textarea.value.trim().split('\n'))];
+    textarea.value = duplicated.join('\n')
+}
